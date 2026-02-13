18818e5152afed0a660244e0b90ca8bcc99b724f
lucasjansen-tech <mmlucasgabriel@gmail.com>
2026-02-13 12:12:07 -0300

    Update omr_engine.py

--- omr_engine.py
+++ omr_engine.py
@@ -17,6 +17,7 @@ def encontrar_ancoras_globais(thresh):
                 M = cv2.moments(c)
                 if M["m00"] != 0:
                     candidatos.append([int(M["m10"]/M["m00"]), int(M["m01"]/M["m00"])])
+    
     if len(candidatos) < 4: return None
     pts = np.array(candidatos, dtype="float32")
     s = pts.sum(axis=1); d = np.diff(pts, axis=1)
@@ -44,14 +45,14 @@ def ler_grid(img_thresh, grid: GridConfig, w_img, h_img, img_debug):
     y1 = int(grid.y_start * h_img)
     y2 = int(grid.y_end * h_img)
     
-    # Desenha a caixa do Grid (Verde)
+    # DEBUG: Quadrado Verde mostrando a área exata de leitura
     cv2.rectangle(img_debug, (x1, y1), (x2, y2), (0, 255, 0), 2)
     
     cell_h = (y2 - y1) / grid.rows
     cell_w = (x2 - x1) / grid.cols
     res_bloco = {}
     
-    # FREQUÊNCIA (Leitura Coluna a Coluna)
+    # Frequência
     if grid.labels == ["D", "U"]:
         freq_res = ["0", "0"]
         for c in range(grid.cols):
@@ -60,26 +61,17 @@ def ler_grid(img_thresh, grid: GridConfig, w_img, h_img, img_debug):
                 cx = int(x1 + (c * cell_w) + (cell_w/2))
                 cy = int(y1 + (r * cell_h) + (cell_h/2))
                 raio = int(min(cell_h, cell_w) * 0.25)
-                
                 roi = img_thresh[cy-raio:cy+raio, cx-raio:cx+raio]
                 col_votos.append(cv2.countNonZero(roi))
-                
-                # Debug ponto de leitura (pequeno ponto azul)
-                cv2.circle(img_debug, (cx, cy), 3, (255, 0, 0), -1)
-            
-            # Quem ganhou na coluna?
             idx_max = np.argmax(col_votos)
-            # Limiar mínimo de tinta
             if max(col_votos) > (raio*raio*0.5):
                 freq_res[c] = str(idx_max)
-                # Pinta o vencedor (Círculo preenchido vermelho)
                 cy_hit = int(y1 + (idx_max * cell_h) + (cell_h/2))
                 cx_hit = int(x1 + (c * cell_w) + (cell_w/2))
-                cv2.circle(img_debug, (cx_hit, cy_hit), int(raio*1.2), (0, 0, 255), -1)
-                
+                cv2.circle(img_debug, (cx_hit, cy_hit), int(raio*1.1), (255, 0, 0), -1)
         return "".join(freq_res), {}
 
-    # QUESTÕES
+    # Questões
     for r in range(grid.rows):
         cy = int(y1 + (r * cell_h) + (cell_h/2))
         densidades = []
@@ -104,7 +96,7 @@ def ler_grid(img_thresh, grid: GridConfig, w_img, h_img, img_debug):
         if grid.questao_inicial > 0:
             res_bloco[grid.questao_inicial + r] = letra
             if marcou:
-                cv2.circle(img_debug, centros[idx_max], int(raio*1.2), (0, 255, 0), 2)
+                cv2.circle(img_debug, centros[idx_max], int(raio*1.1), (0, 255, 0), 2)
 
     return None, res_bloco
 
